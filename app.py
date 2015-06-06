6afbd1a6666651c6be8ba3241ccf2774802d1fc9
Daniel Borstelmann <dborstelmann@gmail.com>
2015-06-06 16:45:36 -0500

    nytimes bitches

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, jsonify
+import requests
 app = Flask(__name__)
+
+
+@app.route("/search/")
+def search():
+    response = requests.get("http://api.nytimes.com/svc/topstories/v1/home.json?api-key=ba5407bdbf571895b000d0faa7948cbe:2:72232947")
+    return jsonify(response.json(), status=200)
 
 @app.route("/")
 def hello():
